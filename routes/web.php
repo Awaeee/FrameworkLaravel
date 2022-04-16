4b6b754ee9698640a2b05b4595b59257491364a8
is42-kayzerovaae <anyakaiserova@gmail.com>
2022-04-16 17:05:49 +0500

    Задание 16.04.22

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Задание 1
+Route::get('my-name', function () {
+    return ('Кайзерова Анна Евгеньевна');
+});
+//Задание 2
+Route::get('my-friend', function () {
+    return ('Косвинцева Ксения Витальевна');
+});
+//Задание 3
+Route::get('get-friend/{name}', function ($name) {
+    $name ='is42-kosvintsevakv';
+    return 'Login: '.$name;
+});
+//Задание 4
+Route::get('my-city/{city}', function ($city) {
+    return $city ;
+});
+//Задание 5
+Route::get('level/{lvl}', function ($lv) {
+
+    if($lv>=0 && $lv<=25){
+        echo 'Новичок!';
+    }
+    if($lv>=26 && $lv<=50){
+        echo 'Специалист!';
+    }
+    if($lv>=51 && $lv<=75){
+        echo 'Босс!';
+    }
+    if($lv>=76 && $lv<=98){
+        echo 'Старик!';
+    }
+    if($lv==99){
+        echo 'Король!';
+    }
+});
+//Задание 6
+
+
